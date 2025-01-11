aa1c310391175b621ee041c99ebcc82d0f4d181d
KoroleffLaszlo <koroleffglaszlo@gmail.com>
2025-01-10 21:07:07 -0800

    feat: client.h update

--- lab1/src/client.cpp
+++ lab1/src/client.cpp
@@ -1,15 +1,11 @@
-#include <sys/socket.h>
 #include <iostream.h>
-#include <netinet/in.h>
 #include <stdlib.h>
 #include <cstring.h>
 #include <cerrno.h>
-#include <arpa/init.h>
-#include <netdb.h>
 
 #include "client.h"
 
-int socket_init(){
+int socket(){
     int soc = socket(AF_INET, SOCK_STREAM, 0);
     if(soc < 0){
         perror("socket_init() Error");
@@ -17,24 +13,35 @@ int socket_init(){
         return -1;
     }
 
-    return 0;
+    return soc;
 }
 
-int connectHost(int soc, struct sockaddr_in &srv_addr, const char* ip, uint_16 port){
+int connect(int soc, struct sockaddr_in &srv_addr, const char* ip, uint_16 port){
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(port)
     if(inet_pton(AF_NET, ip, &srv_addr.sin_addr) < 0){
-        perror("invalid address");
+        perror("Invalid address");
         std::cerr << "Error message: " << strerror(cerr) << std::endl;
         close(soc)
         return -1;
     }
 
     if(connect(soc, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) < 0){
-        perror("connection to server Error");
+        perror("Connection to server Error");
         std::cerr << "Error message: " << strerror(cerr) << std::endl;
         close(soc)
         return -1;
+    }
+
+    return 0;
+}
+
+int send(int soc, const char* request){
+    if(send(soc, request, strlen(request), 0) < 0){
+        perror("get_request() Error");
+        std:cerr << "Error message: " << strerrror(cerr) << std::endl;
+        close(soc)
+        return -1
     }
 
     return 0;
